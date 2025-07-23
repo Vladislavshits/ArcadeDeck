7353b41aca9fd9f35f3c11135f6acb45b61738ba
Vladislavshits <vladislavshits@gmail.com>
2025-07-23 19:43:50 +0300

    Update PixelDeck.py

--- PixelDeck.py
+++ PixelDeck.py
@@ -29,7 +29,7 @@ from PyQt5.QtWidgets import (
     QLabel, QPushButton, QVBoxLayout, QHBoxLayout,  # Виджеты и компоновки
     QStackedWidget, QSizePolicy, QToolButton, QButtonGroup,  # Специальные виджеты
     QCheckBox, QDialog, QTextEdit, QMessageBox, QProgressDialog,  # Диалоги
-    QLineEdit, QListWidget
+    QLineEdit, QListWidget, QListWidgetItem
 )
 
 # Дополнительные компоненты PyQt
