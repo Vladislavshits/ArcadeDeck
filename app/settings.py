f57b6782a13b54da93eea09723798364ebf4e967
Vladislavshits <vladislavshits@gmail.com>
2025-07-27 01:34:31 +0300

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -1,6 +1,7 @@
 from PyQt6.QtCore import QSettings
 from PyQt6.QtWidgets import QApplication
 import os
+from app.ui_assets.theme_manager import theme_manager
 
 class AppSettings:
     def __init__(self):
