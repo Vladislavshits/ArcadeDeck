80ecbafa1c7ab5676ca34ef6b6af3db2c0f17b34
Vladislavshits <vladislavshits@gmail.com>
2025-07-26 19:44:46 +0300

    Update core.py

--- app/core.py
+++ app/core.py
@@ -5,7 +5,7 @@ import re
 from PyQt6.QtGui import QColor, QPalette
 from PyQt6.QtWidgets import QApplication
 from PyQt6.QtCore import Qt
-from settings import app_settings  # Исправленный импорт
+from settings import app_settings
 
 APP_VERSION = "0.1.73-beta"
 USER_HOME = os.path.expanduser("~")
