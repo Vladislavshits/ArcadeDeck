7219055b3e27a1a00d69f8e7b1fa86b7e05fca1e
Vladislavshits <vladislavshits@gmail.com>
2025-08-09 00:42:44 +0300

    0.1.90

--- app/core.py
+++ app/core.py
@@ -3,7 +3,7 @@ import os
 import sys
 from PyQt6.QtWidgets import QApplication
 
-APP_VERSION = "0.1.82-beta"
+APP_VERSION = "0.1.90-beta"
 USER_HOME = os.path.expanduser("~")
 
 # Определяем базовые пути
